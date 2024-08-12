ac1bc2e9430898fe2efbf52a19b8dc7bd90f2211
Deepali Singh <161689749+thd-dev@users.noreply.github.com>
2024-08-12 15:23:56 +0000

    brush and eraser fuctioning done

--- src/index.js
+++ src/index.js
@@ -20,14 +20,15 @@ canvas.style.backgroundColor = '#ffffff'
 
 // Brush, Eraser and paint clicked: 
 
-let draw = false;
+let draw = true;
 let erase = false;
 let fill = false;
 
 brush.addEventListener('click', () => {
-    brush.children[0].children[0].setAttribute('stroke', '#000000')
-    eraser.children[0].children[0].setAttribute('stroke', '#9e9e9e')
+    brush.children[0].children[0].children[0].setAttribute('stroke', '#000000')
+    eraser.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
     paint.children[0].setAttribute('stroke', '#9e9e9e')
+    console.log(brush.children[0].children[0]);
 
     draw = true;
     erase = false;
@@ -35,18 +36,21 @@ brush.addEventListener('click', () => {
 })
 
 eraser.addEventListener('click', () => {
-    eraser.children[0].children[0].setAttribute('stroke', '#000000')
-    brush.children[0].children[0].setAttribute('stroke', '#9e9e9e')
+    eraser.children[0].children[0].children[0].setAttribute('stroke', '#000000')
+    brush.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
     paint.children[0].setAttribute('stroke', '#9e9e9e')
 
+    console.log(eraser.children[0].children[0]);
+    
     draw = false;
     erase = true;
     fill = false;
 })
 paint.addEventListener('click', () => {
-    eraser.children[0].children[0].setAttribute('stroke', '#9e9e9e')
-    brush.children[0].children[0].setAttribute('stroke', '#9e9e9e')
+    eraser.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
+    brush.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
     paint.children[0].setAttribute('stroke', '#000000')
+    console.log(eraser.children[0]);
     
     draw = false;
     erase = false;
@@ -81,15 +85,6 @@ eraserOption.addEventListener('click', () => {
     eraserSize.classList.toggle('hidden')
 }, true)
 
-
-
-// console.log(colorGrid.children.length);
-// for(i = 0 ; i < colorGrid.children.length; i++){
-//     if(colorGrid.children[i].classList.contains('hidden')) continue
-//     console.log(window.getComputedStyle(colorGrid.children[i]).backgroundColor);
-    
-// }
-
 // brush Color
 function rgbToHex(rgb) {
     const rgbValues = rgb.match(/\d+/g);
@@ -100,12 +95,12 @@ let color;
 colorGrid.addEventListener('click',event => {
     let hexCode = rgbToHex(window.getComputedStyle(event.target).backgroundColor);    
     colorGrid.children[colorGrid.children.length - 1].value = hexCode;
+    color = colorGrid.children[colorGrid.children.length - 1].value;
 })
 
 colorGrid.children[colorGrid.children.length - 1].addEventListener('change', event => {
     console.log('color changed');
     color = event.target.value;
-    init()
 })
 
 
@@ -114,42 +109,60 @@ let brushSizeValue = 30;
 let eraserSizValue = 2;
 
 brushSize.addEventListener('change', event => {
-    // console.log(event.target.value);
     brushSizeValue = event.target.value;
 })
 eraserSize.addEventListener('change', event => {
-    console.log(event.target.value);
     eraserSizValue = event.target.value;
 })
 
 // Canvas setup
-const mouse = {
-    x: undefined,
-    y: undefined,
-}
+let isDrawing = false;
+let x = undefined;
+let y = undefined;
 
-canvas.addEventListener('mousemove', event => {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
+canvas.addEventListener('mousedown', event => {
+    isDrawing = true
+    x = event.offsetX;
+    y = event.offsetY;
+})
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false
 })
 
 
 const setUp = () => {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-
-    init();
 }
 
-const init = () => {
-    ctx.fillRect(200, 200, 100, 100)
-    ctx.fillStyle = color;
-    ctx.fill()
-    
+const init = (event) => {
+    if(!isDrawing) return
+
+    if(draw){
+        ctx.lineWidth = brushSizeValue;
+        ctx.lineCap = 'round';
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.quadraticCurveTo(x, y, event.offsetX, event.offsetY);
+        ctx.strokeStyle = color;
+        ctx.stroke()
+
+        x = event.offsetX;
+        y = event.offsetY;
+
+    }else if(erase){
+        ctx.clearRect(event.offsetX, event.offsetY, eraserSizValue, eraserSizValue)
+    }else if(fill){
+
+    }
+
 }
 
 
 
+
+canvas.addEventListener('mousemove', init)
 
 window.addEventListener('resize', setUp)
 setUp();
