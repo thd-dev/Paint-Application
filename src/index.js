f111443767e81ffe6557c9338b7aea257479d814
Deepali Singh <161689749+thd-dev@users.noreply.github.com>
2024-08-13 11:07:10 +0000

    fill function done

--- src/index.js
+++ src/index.js
@@ -28,7 +28,6 @@ brush.addEventListener('click', () => {
     brush.children[0].children[0].children[0].setAttribute('stroke', '#000000')
     eraser.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
     paint.children[0].setAttribute('stroke', '#9e9e9e')
-    console.log(brush.children[0].children[0]);
 
     draw = true;
     erase = false;
@@ -40,7 +39,6 @@ eraser.addEventListener('click', () => {
     brush.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
     paint.children[0].setAttribute('stroke', '#9e9e9e')
 
-    console.log(eraser.children[0].children[0]);
     
     draw = false;
     erase = true;
@@ -50,7 +48,6 @@ paint.addEventListener('click', () => {
     eraser.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
     brush.children[0].children[0].children[0].setAttribute('stroke', '#9e9e9e')
     paint.children[0].setAttribute('stroke', '#000000')
-    console.log(eraser.children[0]);
     
     draw = false;
     erase = false;
@@ -91,7 +88,7 @@ function rgbToHex(rgb) {
     return `#${rgbValues.map(x => parseInt(x).toString(16).padStart(2, '0')).join('')}`;
 }
 
-let color;
+let color = '#000000';
 colorGrid.addEventListener('click',event => {
     let hexCode = rgbToHex(window.getComputedStyle(event.target).backgroundColor);    
     colorGrid.children[colorGrid.children.length - 1].value = hexCode;
@@ -153,15 +150,83 @@ const init = (event) => {
 
     }else if(erase){
         ctx.clearRect(event.offsetX, event.offsetY, eraserSizValue, eraserSizValue)
-    }else if(fill){
-
     }
+}
+canvas.addEventListener('click',  event => {
+    if(fill){
+        const x = event.offsetX;
+        const y = event.offsetY
+        const fillColor = hexToRgb(color);
+        const ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        const targetColor = getPixelColor(x, y, ImageData)        
+        floodfill(x, y, targetColor, fillColor, ImageData)
+    }
+})
 
+function hexToRgb(hex) {
+    let r = parseInt(hex.slice(1, 3), 16);
+    let g = parseInt(hex.slice(3, 5), 16);
+    let b = parseInt(hex.slice(5, 7), 16);
+    return { r, g, b, a: 255 };
 }
 
+const getPixelColor = (x, y, ImageData) => {
+    const index = (y * ImageData.width + x) * 4
 
+    const r = ImageData.data[index]
+    const g = ImageData.data[index + 1]
+    const b = ImageData.data[index + 2]
+    const a = ImageData.data[index + 3]
+    return {r, g, b, a}
+}
 
+const setPixelColor = (x, y, ImageData, fillColor) => {
+    if(!fillColor){
+        console.log('Your color is not defined');
+        return   
+    }
 
+    const index = (y * ImageData.width + x) * 4
+    ImageData.data[index + 0] = fillColor.r;
+    ImageData.data[index + 1] = fillColor.g;
+    ImageData.data[index + 2] = fillColor.b;
+    ImageData.data[index + 3] = fillColor.a;
+}
+
+const colorMatch = (cc, tc) => {
+    return cc.r === tc.r && cc.g === tc.g && cc.b === tc.b && cc.a === tc.a    
+}
+
+const floodfill = (x, y, targetColor, fillColor, ImageData) => {
+    const stack = [{x, y}];
+    const visited = new Set();
+
+    while(stack.length){
+        const {x, y} = stack.pop();
+        const index = (y * ImageData.width + x) * 4;
+
+        if(visited.has(index)) continue;
+        visited.add(index);
+
+        const currentColor = {
+            r : ImageData.data[index],
+            g : ImageData.data[index + 1],
+            b : ImageData.data[index + 2],
+            a : ImageData.data[index + 3],
+        }
+
+        if(!colorMatch(currentColor, targetColor)) continue;
+
+        setPixelColor(x, y, ImageData, fillColor);
+
+        stack.push({x: x - 1, y})
+        stack.push({x: x + 1, y})
+        stack.push({x, y: y - 1})
+        stack.push({x, y: y + 1})
+    }
+
+    ctx.putImageData(ImageData, 0, 0)
+}
 canvas.addEventListener('mousemove', init)
 
 window.addEventListener('resize', setUp)
